1263c6cb77c3a3bb6ed4699b89cfe5600c798628
lynda0214 <lynda0214@gmail.com>
2022-05-24 22:29:36 +0800

    [what] revert df57d0b6c200a54f6aaf85863ee4a000f02f2047

--- src/App.js
+++ src/App.js
@@ -29,12 +29,6 @@ const App = () => {
     const [newComment, setNewComment] = useState(null);
     const [newPicture, setNewPicture] = useState(null);
 
-    useEffect(() => {
-        // preload hand-rock
-        const image = new Image();
-        image.src = `${CURSOR_FOLDER}/hand-rock.png`;
-    }, []);
-
     const handleClick = (event) => {
         setIsClicking(true);
         switch (mode) {
